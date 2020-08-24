be182b996e49f8dfd1b1949bb4a2b01dbe4c0f1e
Radele Rose <radele.rose@gmail.com>
2020-08-24 01:33:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 def move(board, index, current_player = "X")
   board[index.to_i-1] = current_player
 end
